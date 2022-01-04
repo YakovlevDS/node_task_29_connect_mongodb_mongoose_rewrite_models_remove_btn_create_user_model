7977748ba7f0594e8e072b1252ac5d8c79417f57
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-01-04 19:29:15 +0200

    added connect mongodb  mongoose rewrite models remove btn create user model and add user

---
+++ models/course.js
@@ -0,0 +1,22 @@
+const {Schema, model} = require('mongoose')
+
+const course = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  img: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+module.exports = model('Course', course)
